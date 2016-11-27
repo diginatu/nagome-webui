b5120c0a9af1c29e97d736be2efebf11499a2019
diginatu <diginatu.510o@gmail.com>
2016-11-28 01:14:54 +0900

    add menu group title

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Page, List, ListItem, Icon} from 'react-onsenui';
+import {Page, List, ListItem, ListHeader, Icon} from 'react-onsenui';
 import ons from 'onsenui';
 
 import {ngm} from './NagomeConn.js';
@@ -7,37 +7,45 @@ import AccountSetting from './AccountSetting.jsx';
 
 export default class Menu extends Component {
     componentWillMount() {
-        this.menuList = [
-            { text: "Connect to URI",
-                icon: "fa-link",
-                fn: () => {
-                    ons.notification.prompt({
-                        title: 'Connect',
-                        message: 'Input Live ID or URI',
-                        cancelable: true,
-                        callback: (br) => {
-                            ngm.broadConnect(br);
-                        },
-                    });
-                }},
-            {text: "Disconnect",
-                icon: "fa-minus-circle",
-                fn: () => {
-                    ngm.broadDisconnect();
-                }},
-            {text: "Clear comments",
-                icon: "fa-file-o",
-                fn: () => {
-                    ngm.clearComments();
-                }},
-            {text: "Plugins",
-                icon: "fa-file-o",
-                fn: () => {
-                    this.props.navigator.pushPage({
-                        component: AccountSetting,
-                    });
-                }},
-        ];
+        this.menuLists = [{
+            title: "Comment",
+            list: [
+                { text: "Connect to URI",
+                    icon: "fa-link",
+                    fn: () => {
+                        ons.notification.prompt({
+                            title: 'Connect',
+                            message: 'Input Live ID or URI',
+                            cancelable: true,
+                            callback: (br) => {
+                                ngm.broadConnect(br);
+                            },
+                        });
+                    }},
+                {text: "Disconnect",
+                    icon: "fa-minus-circle",
+                    fn: () => {
+                        ngm.broadDisconnect();
+                    }},
+                {text: "Clear comments",
+                    icon: "fa-file-o",
+                    fn: () => {
+                        ngm.clearComments();
+                    }},
+            ],
+        },
+        {
+            title: "settings",
+            list: [
+                {text: "Plugins",
+                    icon: "fa-file-o",
+                    fn: () => {
+                        this.props.navigator.pushPage({
+                            component: AccountSetting,
+                        });
+                    }},
+            ]
+        }];
     }
 
     handleMenuSelect(f) {
@@ -45,22 +53,38 @@ export default class Menu extends Component {
         f();
     }
 
+    renderRow(m) {
+        return (
+            <ListItem key={m.text} onClick={this.handleMenuSelect.bind(this, m.fn)} tappable>
+                <div className="left">
+                    <Icon icon={m.icon} />
+                </div>
+                <div className="center">
+                    {m.text}
+                </div>
+            </ListItem>
+               );
+    }
+
+    renderLists(lists) {
+        return lists.map((list)=>{
+            return (
+                <List
+                    key={list.title}
+                    dataSource={list.list}
+                    renderRow={this.renderRow.bind(this)}
+                    renderHeader={() => 
+                        <ListHeader>{list.title}</ListHeader>
+                    }
+                />
+                );
+        });
+    }
+
     render() {
         return (
             <Page>
-                <List
-                    dataSource={this.menuList}
-                    renderRow={(m) => (
-                        <ListItem key={m.text} onClick={this.handleMenuSelect.bind(this, m.fn)} tappable modifier="nodivider">
-                            <div className="left">
-                                <Icon icon={m.icon} />
-                            </div>
-                            <div className="center">
-                                {m.text}
-                            </div>
-                        </ListItem>
-                    )}
-                />
+                {this.renderLists(this.menuLists)}
             </Page>
             );
     }
