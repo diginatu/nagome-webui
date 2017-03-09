ac1551bc6bb2e35a8843f33b4f6fa6f2787bb1a2
diginatu <diginatu.510o@gmail.com>
2017-03-09 18:28:49 +0900

    add the user art for unnamed users

--- src/CommentList.js
+++ src/CommentList.js
@@ -6,6 +6,51 @@ export default class CommentList extends Component {
     constructor() {
         super();
         this.isBottom = true;
+        this.userArtSeed = 0;
+    }
+
+    djb2_hash(str) {
+        let hash = 5381;
+        for (let i = 0; i < str.length; i++) {
+            // '|0' converts into 32-bit int
+            hash = (((hash << 5) + hash) + str.charCodeAt(i))|0;
+        }
+        this.userArtSeed = hash;
+        return hash;
+    }
+
+    lcg_random(seed = this.userArtSeed) {
+        const a = 22695477;
+        const c = 1;
+        let rand = (seed * a + c)|0;
+        this.userArtSeed = rand;
+        return rand; // '|0' converts into 32-bit int
+    }
+
+    random_color() {
+        const r = this.lcg_random() >>> 2;
+        let colnm = '#' + ('000000' + r.toString(16)).slice(-6);
+        return colnm;
+    }
+
+    renderUserArt(comment) {
+        if (comment.user_name === "") {
+            this.djb2_hash(comment.user_id);
+            return (
+                <table className='user_art'>
+                    <tbody>
+                        <tr>
+                            <td style={{backgroundColor: this.random_color()}}></td>
+                            <td style={{backgroundColor: this.random_color()}}></td>
+                        </tr>
+                        <tr>
+                            <td style={{backgroundColor: this.random_color()}}></td>
+                            <td style={{backgroundColor: this.random_color()}}></td>
+                        </tr>
+                    </tbody>
+                </table>
+            );
+        }
     }
 
     renderRow(row, i) {
@@ -15,6 +60,7 @@ export default class CommentList extends Component {
                     <div className="no">
                         {row.no}
                     </div>
+                    {this.renderUserArt(row)}
                     <div className="user_name">
                         {row.user_name}
                     </div>
@@ -49,7 +95,7 @@ export default class CommentList extends Component {
         return (
             <List
                 dataSource={this.props.data}
-                renderRow={this.renderRow}
+                renderRow={this.renderRow.bind(this)}
                 renderHeader={() => <ListHeader>Comments</ListHeader>} />
         );
     }
